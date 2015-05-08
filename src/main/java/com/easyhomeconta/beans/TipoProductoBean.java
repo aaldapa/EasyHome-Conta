9174deb05c85de8e789b3c396a75eaecb824dba5
Alberto <aaldapa@gmail.com>
2015-05-08 17:48:36 +0200

    Tipo de productos financieros

---
+++ src/main/java/com/easyhomeconta/beans/TipoProductoBean.java
@@ -0,0 +1,64 @@
+/**
+ * 
+ */
+package com.easyhomeconta.beans;
+
+import java.io.Serializable;
+
+/**
+ * @author Alberto
+ *
+ */
+@SuppressWarnings("serial")
+public class TipoProductoBean implements Serializable {
+
+	private Integer idTipoProducto;
+	private String nombre;
+	private Boolean operable;
+	private String notas;
+	
+	public TipoProductoBean() {
+		super();
+	}
+
+	public TipoProductoBean(Integer idProducto, String nombre, String notas, Boolean operable) {
+		super();
+		this.idTipoProducto = idProducto;
+		this.nombre = nombre;
+		this.notas = notas;
+		this.operable = operable;
+	}
+
+	public Integer getIdTipoProducto() {
+		return idTipoProducto;
+	}
+
+	public void setIdTipoProducto(Integer idProducto) {
+		this.idTipoProducto = idProducto;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Boolean getOperable() {
+		return operable;
+	}
+
+	public void setOperable(Boolean operable) {
+		this.operable = operable;
+	}
+
+	public String getNotas() {
+		return notas;
+	}
+
+	public void setNotas(String notas) {
+		this.notas = notas;
+	}
+
+}
